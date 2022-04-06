6c7fa93a164d91076766031a4c7fa5242d59f0a2
Atta Jirofty <jirofty76@gmail.com>
2022-04-05 22:56:11 -0400

    Update SignUp.jsx

--- src/components/Authentication/SignUp.jsx
+++ src/components/Authentication/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
 
 class SignUp extends Component {
@@ -44,55 +44,69 @@ class SignUp extends Component {
 
   render() {
     return (
-      <form
-        className="form col-4 mx-auto"
-        onSubmit={this.submitUser.bind(this)}
-      >
-        <h3>Sign Up</h3>
-        <div className="form-group pb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            ref="firstName"
-          />
-        </div>
-        <div className="form-group pb-3">
-          <label>Last name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Last name"
-            ref="lastName"
-          />
-        </div>
-        <div className="form-group pb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            ref="email"
-          />
-        </div>
-        <div className="form-group pb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            ref="password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary btn-block mb-3">
-          Sign Up
-        </button>
-        <p className="forgot-password text-right">
-          <a href="/login"> Already registered ? Sign in.</a>
-        </p>
-      </form>
+      <Fragment>
+        {this.state.success ? (
+          <section>
+            <h1>You are logged in!</h1>
+            <br />
+            <p>
+              You are signed up. Please go to Login page and use your
+              credentioals.
+            </p>
+          </section>
+        ) : (
+          <form
+            className="form col-4 mx-auto"
+            onSubmit={this.submitUser.bind(this)}
+          >
+            <h3>Sign Up</h3>
+            <div className="form-group pb-3">
+              <label>First name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                ref="firstName"
+              />
+            </div>
+            <div className="form-group pb-3">
+              <label>Last name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                ref="lastName"
+              />
+            </div>
+            <div className="form-group pb-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                ref="email"
+              />
+            </div>
+            <div className="form-group pb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                ref="password"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-block mb-3">
+              Sign Up
+            </button>
+            <p className="forgot-password text-right">
+              <a href="/login"> Already registered ? Sign in.</a>
+            </p>
+          </form>
+        )}
+      </Fragment>
     );
   }
 }
+
 export default SignUp;
